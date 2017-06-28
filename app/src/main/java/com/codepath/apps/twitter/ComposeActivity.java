455c2d5bbbffff41cfb2f468368df9a5c8fdbfbb
AndrianaBurgess <aburgess11@fb.com>
2017-06-27 17:49:56 -0700

    No tweet, but char count working

--- app/src/main/java/com/codepath/apps/twitter/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/twitter/ComposeActivity.java
@@ -1,10 +1,13 @@
 package com.codepath.apps.twitter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class ComposeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,13 @@ public class ComposeActivity extends AppCompatActivity {
 
     public void onSubmit(View v) {
         // closes the activity and returns to first screen
+        Intent i = new Intent(ComposeActivity.this, TimelineActivity.class);
+        // put "extras" into the bundle for access in the second activity
+        i.putExtra("username", "foobar");
+
+        // brings up the second activity
+        startActivity(i);
+
         this.finish();
     }
 }
